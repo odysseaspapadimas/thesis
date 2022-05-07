6fbf20a18b9d24ed839418ac91a8c33f9d58ae88
odysseaspapadimas <odysseas.patra@gmail.com>
2022-05-07 20:51:12 +0300

    changed movies page to static

--- pages/movies.tsx
+++ pages/movies.tsx
@@ -1,16 +1,16 @@
-import { Container, Group } from "@mantine/core";
+import { Container, Group, Skeleton } from "@mantine/core";
 import { useMediaQuery } from "@mantine/hooks";
+import { GetStaticPaths, GetStaticProps } from "next";
 import useSWR from "swr";
 import Show from "../components/Show";
 import { MovieType } from "../constants/types";
 import fetcher from "../helpers/fetcher";
+import { tmdb } from "../utils/tmdb";
 
-const movies = () => {
-  const { data: shows, error } = useSWR(
-    `https://api.themoviedb.org/3/trending/movie/day?api_key=${process.env.NEXT_PUBLIC_TMDB_API_KEY}`,
-    fetcher
-  );
-  const matches = useMediaQuery('(max-width: 400px)', false);
+const movies = ({ movies }: { movies: MovieType[] }) => {
+  const matches = useMediaQuery("(max-width: 400px)", false);
+
+  console.log(movies, "pop movies");
 
   return (
     <Container size="xl" py={36}>
@@ -21,10 +21,12 @@ const movies = () => {
           <div
             className="grid justify-items-center gap-y-2 md:gap-2"
             style={{
-              gridTemplateColumns: matches ? "repeat(auto-fit, minmax(140px, 1fr))" : "repeat(auto-fit, minmax(175px, 1fr))",
+              gridTemplateColumns: matches
+                ? "repeat(auto-fit, minmax(140px, 1fr))"
+                : "repeat(auto-fit, minmax(175px, 1fr))",
             }}
           >
-            {shows?.results?.map((data: MovieType) => (
+            {movies.map((data: MovieType) => (
               <Show key={data.id} data={data} />
             ))}
           </div>
@@ -34,3 +36,17 @@ const movies = () => {
   );
 };
 export default movies;
+
+export const getStaticProps: GetStaticProps = async (ctx) => {
+  const URL = `https://api.themoviedb.org/3/trending/movie/day?api_key=${process.env.NEXT_PUBLIC_TMDB_API_KEY}`;
+
+  const res = await fetch(URL);
+
+  const { results: movies } = await res.json();
+
+  return {
+    props: {
+      movies,
+    },
+  };
+};
