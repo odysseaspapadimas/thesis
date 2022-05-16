29a034a4c20e191f05b5173355133957730b1fcd
odysseaspapadimas <odysseas.patra@gmail.com>
2022-05-16 16:17:51 +0300

    moved /home to /index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,31 +1,61 @@
-import { Container } from "@mantine/core";
-import type { NextPage } from "next";
+import { Button, Container, Loader, Modal, TextInput } from "@mantine/core";
+import { GetStaticProps } from "next";
 import { useSession } from "next-auth/react";
-import Head from "next/head";
-import Image from "next/image";
-import { useRouter } from "next/router";
-import { useEffect } from "react";
+import { useState } from "react";
 import useSWR from "swr";
 import Header from "../components/Header";
+import SignupModal from "../components/Home/SignupModal";
+import TrendingMovies from "../components/Home/TrendingMovies";
+import TrendingShows from "../components/Home/TrendingShows";
+import { MovieType, TVShowType } from "../constants/types";
+import fetcher from "../helpers/fetcher";
+import useUser from "../hooks/use-user";
+import { tmdb } from "../utils/tmdb";
+import { NextPageWithAuth } from "./_app";
 
-const Landing: NextPage = () => {
+interface HomeProps {
+  movies: MovieType[];
+  shows: TVShowType[];
+}
+
+const Home = ({ movies, shows }: HomeProps) => {
   const { data: session } = useSession();
-  const router = useRouter();
 
-  useEffect(() => {
-    if (!session) return;
-
-    router.replace("/home");
-  }, [session, router]);
+  const { data, error } = useSWR(
+    session ? `/api/user/userExists?email=${session.user?.email}` : null,
+    fetcher
+  );
 
   return (
-    <div>
-      <Head>
-        <title>Zetv</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-    </div>
+    <Container size="xl" className="py-10">
+      {data && !data.userExists && (
+        <div>{session && <SignupModal session={session} />}</div>
+      )}
+      <TrendingMovies movies={movies} />
+      <br />
+      <TrendingShows shows={shows} />
+    </Container>
   );
 };
 
-export default Landing;
+
+export default Home;
+
+export const getStaticProps: GetStaticProps = async () => {
+  const { results: movies } = await tmdb.trending({
+    media_type: "movie",
+    time_window: "day",
+  });
+  const { results: shows } = await tmdb.trending({
+    media_type: "tv",
+    time_window: "day",
+  });
+
+  return {
+    props: {
+      movies,
+      shows,
+    },
+    revalidate: 60 * 60 * 24, //24h
+  };
+};
